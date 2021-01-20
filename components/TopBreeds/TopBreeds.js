a1f90753234251f3bcb211bb9bfa6340560e684f
Jonas Lima de Amorim <dev.amorim@outlook.com>
2021-01-19 21:33:41 -0300

    feat: styling top breeds component

--- components/TopBreeds/TopBreeds.js
+++ components/TopBreeds/TopBreeds.js
@@ -3,10 +3,34 @@ import styles from './TopBreeds.module.css';
 const TopBreeds = () => {
   return (
     <div className={styles.top_breeds}>
-      <h2>Most Searched Breeds</h2>
-      <span>66+ breeds for you to discover</span>
-      <div>
-        Breeds
+      <div className={styles.breed_title}>
+        <h2>Most Searched Breeds</h2>
+      </div>
+      <div className={styles.breed_header}>
+        <div className={styles.subtitle}>66+ breeds for you to discover</div>
+        <div className={styles.read_more}><a href="#">See more &rarr;</a></div>
+      </div>
+      <div className={styles.breed_gallery}>
+        <div className={styles.breed_item}>
+          <div className={styles.breed_img} style={{backgroundImage: "url('https://cdn2.thecatapi.com/images/ZocD-pQxd.jpg')"}}>
+          </div>
+          <span className={styles.breed_name}>Bengal</span>
+        </div>
+        <div className={styles.breed_item}>
+          <div className={styles.breed_img} style={{backgroundImage: "url('https://cdn2.thecatapi.com/images/ZocD-pQxd.jpg')"}}>
+          </div>
+          <span className={styles.breed_name}>Bengal</span>
+        </div>
+        <div className={styles.breed_item}>
+          <div className={styles.breed_img} style={{backgroundImage: "url('https://cdn2.thecatapi.com/images/ZocD-pQxd.jpg')"}}>
+          </div>
+          <span className={styles.breed_name}>Bengal</span>
+        </div>
+        <div className={styles.breed_item}>
+          <div className={styles.breed_img} style={{backgroundImage: "url('https://cdn2.thecatapi.com/images/ZocD-pQxd.jpg')"}}>
+          </div>
+          <span className={styles.breed_name}>Bengal</span>
+        </div>
       </div>
     </div>
   )
