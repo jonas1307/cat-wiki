c6b76cd6a7e33a33a500184c347b9c6349eb11f7
Jonas Lima de Amorim <dev.amorim@outlook.com>
2021-02-14 17:46:25 -0300

    infra: adding new routes

---
+++ pages/details/[id].js
@@ -0,0 +1,8 @@
+import Layout from '../../components/Layout/Layout'
+
+export default function Details() {
+  return (
+    <Layout title="Breed details">
+    </Layout>
+  )
+}
---
+++ pages/top.js
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout/Layout";
+
+export default function MostSearched() {
+  return (
+    <Layout title="Top 10">
+
+    </Layout>
+  );
+}
